a7820751d5be3d05f9d3d6e19de47617968b75d2
goutam-reddy <saigouthamreddy.k@gmail.com>
2020-08-10 15:06:44 +0530

    "if loop is added"

--- practise_3.py
+++ practise_3.py
@@ -1,4 +1,9 @@
 print("hello")
 name = input("what is you name ---> ")
 qus =input(f'{name} are you hungry---> ')
-
+if qus == "yes":
+    print("eat first and study")
+elif qus == "no":
+    print("alright then study")
+else :
+    print("give me a proper answer")
