1450d36d654ddb6c0551344da3896df870c1e7e2
goutam-reddy <saigouthamreddy.k@gmail.com>
2020-08-10 15:47:31 +0530

    "i add .lower()"

--- practise_3.py
+++ practise_3.py
@@ -1,6 +1,8 @@
 print("hello")
 name = input("what is you name ---> ")
 qus =input(f'{name} are you hungry---> ')
+qus = qus.lower()
+
 if qus == "yes":
     print("eat first and study")
 elif qus == "no":
