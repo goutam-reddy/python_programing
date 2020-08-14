41fe91972fce421af26d17ba5285a3f771918897
goutam-reddy <saigouthamreddy.k@gmail.com>
2020-08-14 20:30:43 +0530

    converted form name to number

--- logical_oparator_sample.1.py
+++ logical_oparator_sample.1.py
@@ -1,8 +1,8 @@
-name = input("enter your phone.number ---> " )
+name = input("enter your name ---> " )
 if len(name) < 2:
     print("Name must be at least 3 characters.")
 elif len(name) > 14:
-    print("name can be a maximum of 10 characters")
+    print("name can be a maximum of 15 characters")
 else :
-    print("number looks good")
+    print("name looks good")
 
